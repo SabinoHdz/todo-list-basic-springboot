edcefbd7e95e86923da80fac18257c3849b13e7b
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2024-11-09 18:10:09 -0600

    [todo-list]init proyect

---
+++ src/main/java/com/springboot/todolistbasic/todolist_basic/TodolistBasicApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.todolistbasic.todolist_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/todolistbasic/todolist_basic/TodolistBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.todolistbasic.todolist_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
