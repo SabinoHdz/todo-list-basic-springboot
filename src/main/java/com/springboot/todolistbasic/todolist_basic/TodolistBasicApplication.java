3e37858e09aa3aace893ca0c0fb37dce601e42a5
Sabino Hernandez Paulino <sakuraeyes21@gmail.com>
2024-11-23 19:01:11 -0600

    query methods and annotation Query

--- src/main/java/com/springboot/todolistbasic/todolist_basic/TodolistBasicApplication.java
+++ src/main/java/com/springboot/todolistbasic/todolist_basic/TodolistBasicApplication.java
@@ -22,8 +22,32 @@ public class TodolistBasicApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// aplicación de consola
+		List<Person> persons;
 
-		List<Person> persons = personRepository.findAll();
+		System.out.println("-----------------------------------------------");
+		System.out.println("CONSULTA 1: Todos los registros  con findAll()");
+		persons = personRepository.findAll();
+		persons.stream().forEach(person -> System.out.println(person));
+
+		System.out.println("-----------------------------------------------");
+		System.out.println("CONSULTA 2: Todos los registros con findByProgrammingLanguage()");
+		persons = personRepository.findByProgrammingLanguage("Python");
+		persons.stream().forEach(person -> System.out.println(person));
+
+		System.out.println("-----------------------------------------------");
+		System.out
+				.println("CONSULTA 3: Todos los registros con buscarByProgrammingLanguage(programmingLanguage, name)");
+		persons = personRepository.buscarByProgrammingLanguage("Python", "Pepe");
+		persons.stream().forEach(person -> System.out.println(person));
+
+		System.out.println("-----------------------------------------------");
+		System.out.println("CONSULTA 4: Todos los registros con buscarByProgrammingLanguage(programmingLanguage)");
+		persons = personRepository.buscarByProgrammingLanguage("Java");
+		persons.stream().forEach(person -> System.out.println(person));
+
+		System.out.println("-----------------------------------------------");
+		System.out.println("CONSULTA 5: Todos los registros con findByProgrammingLanguageAndName()");
+		persons = personRepository.findByProgrammingLanguageAndName("Java", "Josefa");
 		persons.stream().forEach(person -> System.out.println(person));
 	}
 
--- src/main/java/com/springboot/todolistbasic/todolist_basic/repositories/PersonRepository.java
+++ src/main/java/com/springboot/todolistbasic/todolist_basic/repositories/PersonRepository.java
@@ -1,9 +1,23 @@
 package com.springboot.todolistbasic.todolist_basic.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.springboot.todolistbasic.todolist_basic.entities.Person;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    // nomeclatura de consultas query methods
+    List<Person> findByProgrammingLanguage(String programmingLanguage);
+
+    List<Person> findByProgrammingLanguageAndName(String programmingLanguage, String name);
+
+    // consultas personalizadas de JPA Query
+    @Query("select p from Person p where p.programmingLanguage = ?1")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage);
+
+    @Query("select p from Person p where p.programmingLanguage = ?1 and p.name=?2")
+    List<Person> buscarByProgrammingLanguage(String programmingLanguage, String name);
 
 }
